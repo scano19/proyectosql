1c09c8b44a6125f19bdae7dc1d744fa77924fd54
scano19 <s.cano1973@gmail.com>
2025-08-07 09:13:28 +0200

    Update consulta35.sql

--- consulta35.sql
+++ consulta35.sql
@@ -3,4 +3,4 @@ selecciona todos los actores cuyo primer nombre es 'Johnny'
 */
 select "first_name" as nombre, "last_name" as "apellido"
 from "actor"
-where "first_name" = 'JOHNNY'
+where "first_name" = 'JOHNNY';
